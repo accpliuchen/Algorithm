f9a3ea45a7467da3fdf1e0a27f5db48b959ac5a0
cl6016 <cl6016@nyu.edu>
2021-07-20 16:36:20 +0800

    My Algorithm Interview sort and leetcode

--- Leetcode/src/main/java/com/interview/exercise/exercise1/Test.java
+++ Leetcode/src/main/java/com/interview/exercise/exercise1/Test.java
@@ -1,21 +1,24 @@
 package com.interview.exercise.exercise1;
 
+import java.util.*;
 
+//https://www.cnblogs.com/grandyang/p/4130379.html
 public class Test{
 
-    public int[] twoSum(int[] nums,int target){
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>;
+    public int[] twoSum(int[] nums,int target)
+    {
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
 
 
         int[] res = new int[2];
         for (int i = 0; i < nums.length; ++i) {
-            m.put(nums[i], i);
+            map.put(nums[i], i);
         }
         for (int i = 0; i < nums.length; ++i) {
             int t = target - nums[i];
-            if (m.containsKey(t) && m.get(t) != i) {
+            if (map.containsKey(t) && map.get(t) != i) {
                 res[0] = i;
-                res[1] = m.get(t);
+                res[1] = map.get(t);
                 break;
             }
         }
