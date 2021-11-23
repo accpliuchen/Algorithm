676920ac01d0de5ddc95e3308e4ed5920f568e67
cl6016 <cl6016@nyu.edu>
2021-11-22 23:01:26 -0500

    My Algorithm Interview codeility

--- Leetcode/src/main/java/com/interview/exercise/exercise387/Test.java
+++ Leetcode/src/main/java/com/interview/exercise/exercise387/Test.java
@@ -5,25 +5,25 @@ import java.util.Map;
 
 public class Test {
 
-    public int firstUniqChar(String s){
-        Map<Character,Integer> map=new HashMap<>();
-
-        for(int i=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
-            }else{
-                map.put(s.charAt(i),1);
-            }
-        }
-
-
-        for(int i=0;i<s.length();i++){
-            if(map.get(s.charAt(i))==1)
-                return i;
-        }
-        return -1;
-
-    }
+//    public int firstUniqChar(String s){
+//        Map<Character,Integer> map=new HashMap<>();
+//
+//        for(int i=0;i<s.length();i++){
+//            if(map.containsKey(s.charAt(i))){
+//                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+//            }else{
+//                map.put(s.charAt(i),1);
+//            }
+//        }
+//
+//
+//        for(int i=0;i<s.length();i++){
+//            if(map.get(s.charAt(i))==1)
+//                return i;
+//        }
+//        return -1;
+//
+//    }
 
     public int firstUniqChar(String s){
        for(int i=0;i<s.length();i++){
