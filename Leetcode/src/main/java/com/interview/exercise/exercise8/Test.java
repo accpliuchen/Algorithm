3aaf875c45ad3a1b674d9c4fcb67b5e5444c87f7
cl6016 <cl6016@nyu.edu>
2021-12-11 18:17:42 -0500

    My Algorithm Interview

--- Leetcode/src/main/java/com/interview/exercise/exercise8/Test.java
+++ Leetcode/src/main/java/com/interview/exercise/exercise8/Test.java
@@ -5,38 +5,80 @@ package com.interview.exercise.exercise8;
 public class Test {
 
 
-    public int myAtoi(String str) {
-      if(str==null || str.length()==0) return 0;
-         str=str.trim();
-         char firstChar=str.charAt(0);
-         int sign=1;
-         int start=0;
-         long res=0;
+//    public int myAtoi(String str) {
+//      if(str==null || str.length()==0) return 0;
+//         str=str.trim();
+//         char firstChar=str.charAt(0);
+//         int sign=1;
+//         int start=0;
+//         long res=0;
+//
+//         if(firstChar=='+'){
+//             sign=1;
+//             start++;
+//         }else if(firstChar=='-'){
+//             sign=-1;
+//             start++;
+//        }
+//
+//         for(int i=start;i<str.length();i++){
+//             if(!Character.isDigit(str.charAt(i))){
+//                 return (int) res*sign;
+//             }
+//
+//             res=res*10+str.charAt(i)-'0';
+//             if(sign==1 && res>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+//             if(sign==-1 && res>Integer.MAX_VALUE) return Integer.MIN_VALUE;
+//         }
+//
+//         return (int)res*sign;
+//
+//    }
 
-         if(firstChar=='+'){
-             sign=1;
-             start++;
-         }else if(firstChar=='-'){
-             sign=-1;
-             start++;
+    public int myAtoi(String str) {
+        if(str.length()==0 || str==null)
+            return 0;
+
+        int index=0;
+
+        //remove white space
+        while(index<str.length() && str.charAt(index)==' '){
+            index++;
         }
 
-         for(int i=start;i<str.length();i++){
-             if(!Character.isDigit(str.charAt(i))){
-                 return (int) res*sign;
-             }
+        if(index==str.length()){
+            return 0;
+        }
 
-             res=res*10+str.charAt(i)-'0';
-             if(sign==1 && res>Integer.MAX_VALUE) return Integer.MAX_VALUE;
-             if(sign==-1 && res>Integer.MAX_VALUE) return Integer.MIN_VALUE;
-         }
+        int sign=1;
 
-         return (int)res*sign;
+        if(str.charAt(index)=='+' || str.charAt(index)=='-'){
+            sign=str.charAt(index)=='+'?1:-1;
+            index++;
+        }
 
+        int res=0;
+
+        while(index<str.length()){
+            int digit=str.charAt(index)-'0';
+
+            if(digit<0 || digit>9) break;
+
+            int newRes=res*10+digit;
+
+            if(Integer.MAX_VALUE/10<res || (Integer.MAX_VALUE/10==res && Integer.MAX_VALUE%10<digit))
+                return  sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+
+            res=newRes;
+            index++;
+        }
+
+        return res*sign;
     }
 
     public static void main(String args[]){
-        String str="-91283472332";
+//        String str="-91283472332";
+        String str="-42";
 
         Test test=new Test();
         System.out.println(test.myAtoi(str));
