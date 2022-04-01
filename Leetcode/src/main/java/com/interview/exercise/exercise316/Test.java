596c6ecb196a0fb7cfc76911686b25de4b259715
cl6016 <cl6016@nyu.edu>
2022-04-01 10:21:16 -0400

    My Algorithm Interview

---
+++ Leetcode/src/main/java/com/interview/exercise/exercise316/Test.java
@@ -0,0 +1,21 @@
+package com.interview.exercise.exercise316;
+
+import java.util.Stack;
+
+//https://www.cnblogs.com/cnoodle/p/13776226.html
+public class Test {
+
+//    public String removeDuplicateLetters(String s) {
+//        int[] count = new int[26];
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        String str = "bcabc";
+//
+//        Test test=new Test();
+//        String result=test.removeDuplicateLetters(str);
+//
+//        System.out.println("result value is "+ result);
+//    }
+}
