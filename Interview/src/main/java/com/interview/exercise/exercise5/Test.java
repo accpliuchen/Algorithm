157348b335c679c564687d00310f9cbe5d06802c
cl6016 <cl6016@nyu.edu>
2022-05-23 02:18:09 -0400

    upload testing code

--- Interview/src/main/java/com/interview/exercise/exercise5/Test.java
+++ Interview/src/main/java/com/interview/exercise/exercise5/Test.java
@@ -4,9 +4,38 @@ import java.util.Stack;
 
 public class Test {
 
-    public static void main(String args){
 
+    public static Stack<Integer> sortStackByStack(Stack<Integer> stack){
+        Stack<Integer> help=new Stack<Integer>();
 
+        while(!stack.isEmpty()){
+            int cur=stack.pop();
+            if(!help.isEmpty() && help.peek()<cur){
+                stack.push(help.pop());
+            }
+
+            help.push(cur);
+        }
+
+        while(!help.isEmpty()){
+           stack.push(help.pop());
+        }
+        return stack;
+    }
+
+    public static void main(String args[]){
+
+        Stack<Integer> stack=new Stack<>();
+        stack.push(4);
+        stack.push(8);
+        stack.push(7);
+        stack.push(5);
+
+        Stack<Integer> item=Test.sortStackByStack(stack);
+
+        for(Integer items:item){
+            System.out.println(items);
+        }
 
     }
 }
