2d2091a68eb51a64c93281d2a05b082f66fb09d0
cl6016 <cl6016@nyu.edu>
2023-10-29 21:16:33 -0400

    laioffer and leetcode

---
+++ Leetcode/src/main/java/com/interview/exercise/exercise227/Test.java
@@ -0,0 +1,41 @@
+package com.interview.exercise.exercise227;
+
+public class Test {
+
+    public int calculate(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+    }
+
+    public static void main(String args[]){
+        String str="3+2*2";
+
+        Test test=new Test();
+
+        int result=test.calculate(str);
+
+        System.out.println(result);
+    }
+}
